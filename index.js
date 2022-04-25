68cf1ceef28a98bd23f20f115ca357bfd605cbc5
Abhishekkp42 <96106223+Abhishekkp42@users.noreply.github.com>
2022-04-25 23:03:22 +0530

    final changes

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 require('dotenv').config()
 const express = require('express')
 const app = express();
-const port = `${process.env.PORT}` || 5000;
+const port = process.env.PORT || 8000;
 const cors = require('cors');
 app.use(express.json())
 app.use(cors())
@@ -22,7 +22,7 @@ app.listen(port, async() => {
         await connect()
         console.log(`listing to the port ${port}`)
     } catch (error) {
-        console.log('error:', error)
+        console.log('error:', error.message)
         
     }
 })
--- src/configs/db.js
+++ src/configs/db.js
@@ -1,8 +1,6 @@
 const mongoose = require('mongoose');
 
-const connect = () => {
-return mongoose.connect(`mongodb://localhost:27017/petboarding`);
+module.exports = () => {
+return mongoose.connect("mongodb://127.0.0.1:27017/petboarding");
 
 }
-
-module.exports = connect
